1516c51a328db38330760dbf30880665afa5e909
avikarastogi <avikarastogi20092gmai.com>
2025-07-16 14:14:07 -0500

    s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const font = Poppins({ weight: '700', subsets: ['latin']})
 const title = 'Avika\'s Site';
 const description = 'This my personal site';
 const links = [
-  {title: 'All About Me', href: '/All About Me'},
-  {title: 'Velocity', href: '/Velocity'}
+  {title: 'All About Me', href: '/pokemon'},
+  {title: 'Velocity', href: '/rick-and-morty'}
 ];
 const SocialLinks = {
   github: '#github',
