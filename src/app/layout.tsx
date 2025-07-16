491088fc95cfc078096570d36465e507b5f93a6a
avikarastogi <avikarastogi20092gmai.com>
2025-07-16 13:12:40 -0500

    COMMIT

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ TODO:  Change these things along with:
   - favicon.ico in /public
  */
 const font = Poppins({ weight: '700', subsets: ['latin']})
-const title = 'Avikas\'s Site';
+const title = 'Avika\'s Site';
 const description = 'This my personal site';
 const links = [
   {title: 'Pokemon', href: '/pokemon'},
